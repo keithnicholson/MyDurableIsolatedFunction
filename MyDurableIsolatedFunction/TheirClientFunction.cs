fb55e5dabb164a3e11abc4a19b74b6e9e5772518
Keith Nicholson <knicholson@ksdynamics.com>
2023-08-29 05:09:38 -0500

    Works based on the other code using the woker extension and not the DurableTask nuget package directly.

--- MyDurableIsolatedFunction/TheirClientFunction.cs
+++ MyDurableIsolatedFunction/TheirClientFunction.cs
@@ -1,6 +1,8 @@
 using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.DurableTask;
+using Microsoft.DurableTask.Client;
 using Microsoft.Extensions.Logging;
 
 namespace MyDurableIsolatedFunction
@@ -25,6 +27,46 @@ namespace MyDurableIsolatedFunction
             response.WriteString("Welcome to Their Azure Functions!");
 
             return response;
+        }
+
+        [Function("IsolatedHttpTriggerDurableStarter")]
+        public async Task<HttpResponseData> HttpTriggerDurableStarter(
+            [HttpTrigger(AuthorizationLevel.Anonymous,"get","post")] HttpRequestData req,
+            [DurableClient] DurableTaskClient client)
+        {
+            _logger.LogInformation("Keith trigger processed");
+
+            string[] payload = { "Oklahoma City", "Stillwater", "Austin" };
+            string instanceId = await client.ScheduleNewOrchestrationInstanceAsync(nameof(Orchestration), payload);
+
+            _logger.LogInformation("Marvin orch");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            response.WriteString("Nicholson Welcome");
+
+            return response;
+        }
+
+        [Function(nameof(Orchestration))]
+        public async Task Orchestration(
+            [OrchestrationTrigger] TaskOrchestrationContext context,
+            string[] payload)
+        {
+
+            _logger.LogInformation("Starting orchestration with instance ID = {instanceId}", context.InstanceId);
+            foreach (string name in payload)
+            {
+                _logger.LogInformation("Starting activity for name = {name}", name);
+                string response = await context.CallActivityAsync<string>(nameof(HelloCities), name);
+            }
+        }
+
+        [Function(nameof(HelloCities))]
+        public string HelloCities([ActivityTrigger] string cityName, FunctionContext executionContext)
+        {
+            _logger.LogInformation("Saying hello to {name}", cityName);
+            return $"Hello, {cityName}!";
         }
     }
 }
